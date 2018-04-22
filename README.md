00db19739f2a8ad49b7b9f76a3f98243c482d7bc
James Bradbury <jekbradbury@gmail.com>
2018-04-21 22:44:50 -0400

    WIP native interface

---
+++ README.md
@@ -0,0 +1,3 @@
+# SpaCy.jl
+
+Julia interface to [SpaCy](https://github.com/explosion/spacy), the Cython NLP package. Work in progress.
--- src/SpaCy.jl
+++ src/SpaCy.jl
@@ -4,6 +4,7 @@ module SpaCy
 export load
 
 using PyCall
+using PyCall: PyObject_struct
 
 const spacy = PyNULL()
 const spacy_cli = PyNULL()
@@ -11,33 +12,33 @@ const spacy_cli = PyNULL()
 function __init__()
     copy!(spacy, pyimport_conda("spacy", "spacy"))
     copy!(spacy_cli, pyimport_conda("spacy.cli", "spacy"))
-    PyCall.pytype_mapping(spacy["tokens"]["doc"]["Doc"], Doc)
-    PyCall.pytype_mapping(spacy["tokens"]["token"]["Token"], Token)
-    PyCall.pytype_mapping(spacy["tokens"]["span"]["Span"], Span)
-    #PyCall.pytype_mapping(spacy.tokens[:lexeme][:Lexeme], Lexeme)
+    PyCall.pytype_mapping(spacy["tokens"]["Doc"], Doc)
+    PyCall.pytype_mapping(spacy["tokens"]["Token"], Token)
+    PyCall.pytype_mapping(spacy["tokens"]["Span"], Span)
+    PyCall.pytype_mapping(spacy["lexeme"]["Lexeme"], Lexeme)
 end
 
-struct Language
+struct PyLanguage
     o::PyObject
 end
 
-struct Doc
+struct PyDoc
     o::PyObject
 end
 
-struct Token
+struct PyToken
     o::PyObject
 end
 
-struct Span
+struct PySpan
     o::PyObject
 end
 
-struct Lexeme
+struct PyLexeme
     o::PyObject
 end
 
-for T in (Language, Doc, Token, Span, Lexeme)
+for T in (PyLanguage, PyDoc, PyToken, PySpan, PyLexeme)
     @eval begin
         PyCall.PyObject(f::$T) = f.o
         Base.convert(::Type{$T}, o::PyObject) = $T(o)
@@ -65,9 +66,121 @@ function load(s::AbstractString)
     catch
         spacy_cli["download"](s)
         spacy["load"](s)
-    end |> Language
+    end |> PyLanguage
 end
 
-(lang::Language)(s) = Doc(lang.o(s))
+(lang::PyLanguage)(s) = reinterpret(Doc, lang.o(s))
+
+const flags_t = UInt64
+const attr_t = UInt64
+const hash_t = UInt64
+const POS_enum = Int32 # ?
+const Cbool = Int32
+
+struct LexemeC # doc.c:1811 __pyx_t_5spacy_7structs_LexemeC
+    flags::flags_t
+    lang::attr_t
+    id::attr_t
+    length::attr_t
+    orth::attr_t
+    lower::attr_t
+    norm::attr_t
+    shape::attr_t
+    prefix::attr_t
+    suffix::attr_t
+    cluster::attr_t
+    prob::Float32
+    sentiment::Float32
+end
+
+struct TokenC # doc.c:1859 __pyx_t_5spacy_7structs_TokenC
+    lex::Ptr{LexemeC} # struct __pyx_t_5spacy_7structs_LexemeC const *lex;
+    morph::UInt64 # uint64_t morph;
+    pos::POS_enum # enum __pyx_t_5spacy_15parts_of_speech_univ_pos_t pos;
+    spacy::Cbool # int spacy;
+    tag::attr_t # __pyx_t_5spacy_8typedefs_attr_t tag;
+    idx::Int32 # int idx;
+    lemma::attr_t # __pyx_t_5spacy_8typedefs_attr_t lemma;
+    sense::attr_t # __pyx_t_5spacy_8typedefs_attr_t sense;
+    head::Int32 # int head;
+    dep::attr_t # __pyx_t_5spacy_8typedefs_attr_t dep;
+    l_kids::UInt32 # uint32_t l_kids;
+    r_kids::UInt32 # uint32_t r_kids;
+    l_edge::UInt32 # uint32_t l_edge;
+    r_edge::UInt32 # uint32_t r_edge;
+    sent_start::Int32 # int sent_start;
+    ent_iob::Int32 # int ent_iob;
+    ent_type::attr_t # __pyx_t_5spacy_8typedefs_attr_t ent_type;
+    ent_id::hash_t # __pyx_t_5spacy_8typedefs_hash_t ent_id;
+end
+
+struct Vocab
+    ob_base::PyObject # PyObject_HEAD
+    vtable::Ptr{Cvoid}
+    mem::Ptr{Cvoid} # cymem.cymem.Pool
+    strings::Ptr{Cvoid} # spacy.strings.StringStore
+    morphology::Ptr{Cvoid} # spacy.morphology.Morphology
+    vectors::PyPtr # array?
+    length::Int32
+    data_dir::PyPtr
+    lex_attr_getters::PyPtr
+    cfg::PyPtr
+    _by_hash::Ptr{Cvoid} # preshed.maps.PreshMap
+    _by_orth::Ptr{Cvoid} # preshed.maps.PreshMap
+end
+
+struct Doc # doc.c:2526
+    ob_base::PyObject_struct # PyObject_struct_HEAD
+    vtable::Ptr{Cvoid} # struct __pyx_vtabstruct_5spacy_6tokens_3doc_Doc *__pyx_vtab;
+    mem::Ptr{Cvoid} # struct __pyx_obj_5cymem_5cymem_Pool *mem;
+    vocab::Ptr{Vocab} # struct __pyx_obj_5spacy_5vocab_Vocab *vocab;
+    _vector::PyPtr # PyObject_struct *_vector;
+    _vector_norm::PyPtr # PyObject_struct *_vector_norm;
+    tensor::PyPtr # PyObject_struct *tensor;
+    cats::PyPtr # PyObject_struct *cats;
+    user_data::PyPtr # PyObject_struct *user_data;
+    c::Ptr{TokenC} # struct __pyx_t_5spacy_7structs_TokenC *c;
+    is_tagged::Cbool # int is_tagged;
+    is_parsed::Cbool # int is_parsed;
+    sentiment::Float32 # float sentiment;
+    user_hooks::PyPtr # PyObject_struct *user_hooks;
+    user_token_hooks::PyPtr # PyObject_struct *user_token_hooks;
+    user_span_hooks::PyPtr # PyObject_struct *user_span_hooks;
+    _py_tokens::PyPtr # PyObject_struct *_py_tokens;
+    length::Int32 # int length;
+    max_length::Int32 # int max_length;
+    noun_chunks_iterator::PyPtr # PyObject_struct *noun_chunks_iterator;
+    __weakref__::PyPtr # PyObject_struct *__weakref__;
+end
+
+struct Span
+    ob_base::PyObject_struct # PyObject_struct_HEAD
+    vtable::Ptr{Cvoid}
+    doc::Ptr{Doc}
+    start::Int32
+    end_::Int32
+    start_char::Int32
+    end_char::Int32
+    label::attr_t
+    _vector::PyPtr
+    _vector_norm::PyPtr
+end
+
+struct Token
+    ob_base::PyObject_struct # PyObject_struct_HEAD
+    vtable::Ptr{Cvoid}
+    vocab::Ptr{Vocab} # cdef readonly Vocab vocab
+    c::Ptr{TokenC} # cdef TokenC* c
+    i::Int32 # cdef readonly int i
+    doc::Ptr{Doc} # cdef readonly Doc doc
+end
+
+struct Lexeme
+    ob_base::PyObject_struct # PyObject_struct_HEAD
+    vtable::Ptr{Cvoid}
+    c::Ptr{LexemeC} # cdef LexemeC* c
+    vocab::Ptr{Vocab} # cdef readonly Vocab vocab
+    orth::attr_t # cdef readonly attr_t orth
+end
 
 end # module
